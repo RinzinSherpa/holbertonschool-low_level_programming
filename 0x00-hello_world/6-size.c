c07200761a5fe1d2634806e253d17f3ab8d34777
Wukongzin <2rinzin1@gmail.com>
2018-06-21 01:17:03 +0000

    corrected long

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -15,7 +15,7 @@ int main(void)
 
 	printf("Size of a char: %ld byte(s)\n", sizeof(charType));
 	printf("Size of an int: %zu byte(s)\n", sizeof(intType));
-	printf("Size of a longe: %ld byte(s)\n", sizeof(longintType));
+	printf("Size of a long: %ld byte(s)\n", sizeof(longintType));
 	printf("Size of a long long: %ld byte(s)\n", sizeof(longlongType));
 	printf("Size of a float: %ld byte(s)\n", sizeof(floatType));
 	return (0);

e6ca0dba3c1910ba74dfe84067e4f38e924ecb9a
Wukongzin <2rinzin1@gmail.com>
2018-06-21 00:46:03 +0000

    corrected the spaces

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -5,15 +5,17 @@
  *
  * Return: Always 0 (Success)
  */
-int main()
+int main(void)
 {
 	char charType;
+	int intType;
+	long int longintType;
+	long long longlongType;
 	float floatType;
-	long longType;
-	long longlongType; 
-	printf("Size of char: %ld bytes\n",sizeof(charType));
-	printf("Size of float: %ld bytes\n",sizeof(floatType));
-	printf("Size of longe: %ld bytes\n",sizeof(longType));
-	printf("Size of longlong: %ld byte\n",sizeof(longlongType));
-	return(0);
+	printf("Size of char: %ld bytes\n", sizeof(charType));
+	printf("size of int: %zu bytes\n", sizeof(intType));
+	printf("Size of longe: %ld bytes\n", sizeof(longintType));
+	printf("Size of longlong: %ld byte\n", sizeof(longlongType));
+	printf("Size of float: %ld bytes\n", sizeof(floatType));
+	return (0);
 }

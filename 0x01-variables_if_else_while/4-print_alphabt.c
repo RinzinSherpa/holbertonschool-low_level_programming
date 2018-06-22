43dd737c995234611289b786eee6d180b2654b4d
Wukongzin <2rinzin1@gmail.com>
2018-06-22 05:37:28 +0000

    corrected the space

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -1,22 +1,22 @@
-#include <stdlib.h>
+#include <stdib.h>
 #include <time.h>
 #include <stdio.h>
-/**                                                                          
-* main - Entry point                                                           
-* This is the preferred style for multi-line                                   
-* Return: Always 0 (Success)                                                  
+/**
+*main - Entry point*
+*This is the preferred style for multi -line
+*Return: Always 0 (Success)
 */
 
 int main(void)
 {
-	char c;
+  char c;
 
-	for (c='a';c<='z';c++)
+  for (c = 'a'; c <= 'z'; c++)
+    {
+	if (c == 'e' || c == 'q')
 	{
-	if(c=='e'||c=='q')
-	{
-	c++;
+	  c++;
 	}
 	putchar(c);
-}
+	}
 }

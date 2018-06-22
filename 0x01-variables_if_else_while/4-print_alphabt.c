1a89ca4ed9d73fed47062e09dacb172160115e58
Wukongzin <2rinzin1@gmail.com>
2018-06-22 05:46:19 +0000

    correct

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -9,14 +9,15 @@
 
 int main(void)
 {
-  char c;
-
-  for (c = 'a'; c <= 'z'; c++)
-    {
+	char c;
+	for (c = 'a'; c <= 'z'; c++)
+	{
 	if (c == 'e' || c == 'q')
 	{
 	  c++;
 	}
 	putchar(c);
 	}
+	putchar('\n');
+	return (0);
 }

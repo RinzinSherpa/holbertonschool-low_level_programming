fcdb54ece8df92a9b484860983e81c88f8ab0aaf
Wukongzin <2rinzin1@gmail.com>
2018-06-22 05:10:23 +0000

    added the condition inside the loop

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,22 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/**                                                                          
+* main - Entry point                                                           
+* This is the preferred style for multi-line                                   
+* Return: Always 0 (Success)                                                  
+*/
+
+int main(void)
+{
+	char c;
+
+	for (c='a';c<='z';c++)
+	{
+	if(c=='e'||c=='q')
+	{
+	c++;
+	}
+	putchar(c);
+}
+}

b0d05bb0955ff7e7befa20984757b91f6fb73e0c
Wukongzin <2rinzin1@gmail.com>
2018-06-23 02:04:11 +0000

    edited the extension

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -8,17 +8,17 @@
  */
 int main(void)
 {
-	int n;
+        int n;
 
-	for (n = '0'; n >= '9'; n++)
-	{
-		putchar(n);
-	}
-	for (n = 'a'; n >= 'f'; n++)
-	{
-		putchar(n);
-	}
+        for (n = '0'; n <= '9'; n++)
+        {
+            putchar(n);
+        }
+        for (n ='a' ; n<='f'; n++)
+        {
+            putchar(n);
+        }
 
-	putchar('\n');
-	return (0);
+        putchar('\n');
+        return (0);
 }

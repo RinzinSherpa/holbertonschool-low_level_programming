47c8b508a9eb2177cfb1bb9e6a3b832fd01db09b
Wukongzin <2rinzin1@gmail.com>
2018-06-23 02:31:38 +0000

    corrected the white spaces

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -8,17 +8,18 @@
  */
 int main(void)
 {
-        int n;
+	int n;
 
-        for (n = '0'; n <= '9'; n++)
-        {
-            putchar(n);
-        }
-        for (n ='a' ; n<='f'; n++)
-        {
-            putchar(n);
-        }
+	for (n = '0'; n <= '9'; n++)
+	{
 
-        putchar('\n');
-        return (0);
+		putchar(n);
+	}
+	for (n = 'a'; n < = 'f'; n++)
+	{
+		putchar(n);
+	}
+	putchar('\n');
+	return (0);
 }
+

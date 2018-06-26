2e1ad935aaf393cf3a08b73a4e3fd5f112f39f4d
Wukongzin <2rinzin1@gmail.com>
2018-06-26 16:19:54 +0000

    added endif

--- 0x02-functions_nested_loops/holberton.h
+++ 0x02-functions_nested_loops/holberton.h
@@ -1,15 +1,13 @@
-#ifndef _HOLBERTON_H_
-#define _HOLBERTON_H_
-/**
- * _putchar - writes the character c to stdout
+#ifndef __HOLBERTON_h__
+#define _HOLBERTON_h__
 
+/*
+ * Write a program that prints Holberton, followed by a new line.
+ *  Function prototypes
 */
 
 int _putchar(char c);
-
-
-
+void print_alphabet(void);
 
 
 #endif
-

02b83ddac375e57958252138ab63b3cf6332e6d7
Wukongzin <2rinzin1@gmail.com>
2018-06-26 05:19:38 +0000

    corrected the header

--- 0x02-functions_nested_loops/0-holberton.c
+++ 0x02-functions_nested_loops/0-holberton.c
@@ -1,1 +1,18 @@
 #include "holberton.h"
+int main(void)
+{
+		/**_putchar s[10]="holberton\n";*/
+
+	_putchar('H');
+	_putchar('o');
+	_putchar('l');
+	_putchar('b');
+	_putchar('e');
+	_putchar('r');	
+	_putchar('t');
+	_putchar('o');
+	_putchar('n');
+
+	_putchar('\n');
+return(0);
+}

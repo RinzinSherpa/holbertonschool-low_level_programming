96746569f9141dafc4e4059bf68ebcedd7be3df0
Wukongzin <2rinzin1@gmail.com>
2018-06-28 22:37:51 +0000

    prints the number from 0 to 9 followed by new line

---
+++ 0x03-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,15 @@
+#include "holberton.h"
+/**
+ * void print_numbers(void) - function that prints the number from 0 to 9
+*/
+void print_numbers(void)
+{
+	int x;
+
+	for (x = '0'; x <= '9'; x++)
+	{
+		_putchar (x);
+	}
+		_putchar ('\n');
+	
+}

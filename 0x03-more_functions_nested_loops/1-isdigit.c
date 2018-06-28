f6f8f2d4497add4e5b455621f30a9283d9fac325
Wukongzin <2rinzin1@gmail.com>
2018-06-28 21:25:49 +0000

    checks for a digit (0 through 9)

---
+++ 0x03-more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,19 @@
+#include "holberton.h"
+/**
+ * _isdigit - function that checks for a digit (0 through 9)
+ * @c: parameter is c
+ * Return: return type is int
+*/
+int _isdigit(int c)
+{
+
+	if (c >= '0' && c <= '9')
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+}
+

8339f05760c0c4c5c56b26ec0c3a9ef2d98636c9
Wukongzin <2rinzin1@gmail.com>
2018-06-28 20:36:43 +0000

    prototype listed

---
+++ 0x03-more_functions_nested_loops/holberton.h
@@ -0,0 +1,15 @@
+#ifndef _HOLBERTON_H_
+#define _HOLBERTON_H_
+
+/**
+ * Write a program that prints Holberton, followed by a new line.
+ *  Function prototypes
+ */
+
+int _isupper(int c);
+
+
+
+
+
+#endif

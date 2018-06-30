327b85f09c2b0111afbef69c792cb73c1e90dad0
Wukongzin <2rinzin1@gmail.com>
2018-06-30 01:49:39 +0000

    % of 3 & 5 interview question

---
+++ 0x03-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+/**
+ * main - function
+*/
+int main(void)
+{
+	int y;
+
+	for (y = 1; y <= 100; y++)
+	{
+		if (y % 5 == 0 && y % 3 == 0)
+		{
+			printf("FizzBuzz");
+		}
+		else if (y  % 5 == 0)
+		{
+			printf("Buzz");
+		}
+
+		else if (y % 3 == 0)
+		{
+			printf("Fizz");
+		}
+		else
+		{	
+			printf("%d", y);			
+		}
+		if (y != 100)		
+		{
+			printf(" ");
+		}
+	}
+	printf("\n");
+	return (0);
+}

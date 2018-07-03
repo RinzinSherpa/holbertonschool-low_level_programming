fcf262838d7d1607dfc5f604907a82bb26a54659
Wukongzin <2rinzin1@gmail.com>
2018-07-03 16:52:31 +0000

    takes a pointer to an int and updates value

---
+++ 0x04-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,12 @@
+#include "holberton.h"
+/**
+ * reset_to_98 - Takes pointer to int as parameter and updates the value to 98
+ * @n: parmeter is *n
+ */
+
+void reset_to_98(int *n)
+{
+
+	*n = 98;
+
+}

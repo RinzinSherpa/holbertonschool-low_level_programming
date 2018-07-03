1c3a26d5f19f2fe6d1d2226e82a49ff9096054e7
Wukongzin <2rinzin1@gmail.com>
2018-07-03 16:57:01 +0000

    prototype added

---
+++ 0x04-pointers_arrays_strings/holberton.h
@@ -0,0 +1,31 @@
+#ifndef _HOLBERTON_H_
+#define _HOLBERTON_H_
+
+/**
+ * Write a program that prints Holberton, followed by a new line.
+ *  Function prototypes
+ */
+
+int _isupper(int c);
+int _isdigit(int c);
+int mul(int a, int b);
+int _putchar(char c);
+void print_numbers(void);
+void print_most_numbers(void);
+void more_numbers(void);
+void print_line(int n);
+void print_diagonal(int n);
+void print_square(int size);
+void print_triangle(int size);
+
+
+void reset_to_98(int *n);
+
+
+
+
+
+
+
+
+#endif

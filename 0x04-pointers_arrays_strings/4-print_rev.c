44e9b151e33ea5d8f14649ddbf79632156118166
Wukongzin <2rinzin1@gmail.com>
2018-07-04 03:12:05 +0000

    function that reverses the string

---
+++ 0x04-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,22 @@
+#include "holberton.h"
+
+/**
+ *print_rev - function that prints a string followed by new line
+ *@s: parmeter is s
+ */
+
+void print_rev(char *s)
+{
+	int i;
+
+
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		;
+	}
+	for (; s[i] >= 0; i--)
+	{
+		_putchar(s[i]);
+	}
+	_putchar('\n');
+}

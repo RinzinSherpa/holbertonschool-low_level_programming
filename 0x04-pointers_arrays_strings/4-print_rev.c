a70267ed77884c70bce38b53be57953f151c303e
Wukongzin <2rinzin1@gmail.com>
2018-07-04 05:42:40 +0000

    prints the string in reverse

--- 0x04-pointers_arrays_strings/4-print_rev.c
+++ 0x04-pointers_arrays_strings/4-print_rev.c
@@ -3,21 +3,20 @@
 /**
  *print_rev - prints a string followed by new line
  *@s: parmeter is s
- *@i: parameter is i
  */
 
 void print_rev(char *s)
 {
 	int i;
 
-
 	for (i = 0; s[i] != '\0'; i++)
 	{
-		;
+
 	}
-	for (i = i - 1; s[i] >= 0; i--)
+	for (i = i - 1; i >= 0; --i)
 	{
 		_putchar(s[i]);
 	}
 	_putchar('\n');
 }
+

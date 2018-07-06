f1d7954165ef74097fe93350be8ecd08099d363d
Wukongzin <2rinzin1@gmail.com>
2018-07-06 22:25:58 +0000

    function that concatenates two strings

--- 0x05-pointers_arrays_strings/1-strncat.c
+++ 0x05-pointers_arrays_strings/1-strncat.c
@@ -3,20 +3,21 @@
  *_strncat - function that concatenates two strings
  *@dest: parameter is *dest of type char
  *@src: parameter is *src of type char
- *@i: paramet is n of type integer
- *Returns - returns the output of type char
+ *@n: paramet is n of type integer
+ *Return: returns dest
  */
 
 char *_strncat(char *dest, char *src, int n)
 {
 	int length;
 	int s2;
+	int n;
 
 	for (length = 0; dest[length] != '\0'; length++)
 	{
 		;
 	}
-	for (s2 =0 ; src[s2] != '\0' && s2 < n; s2++)
+	for (s2 = 0 ; src[s2] != '\0' && s2 < n; s2++)
 	{
 		dest[length + s2] = src[s2];
 	}

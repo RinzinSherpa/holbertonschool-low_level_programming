7d63bd3723beacb14073e35e7b8da7b17732e0af
Wukongzin <2rinzin1@gmail.com>
2018-07-06 23:36:26 +0000

    write a function that copies a string

---
+++ 0x05-pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,23 @@
+#include "holberton.h"
+/**
+ * _strncpy - function copies the string
+ * @dest: dest is the pointer of type char
+ * @src: src is the pointer of type char
+ * @n: n is the variable of type integer
+ * Return:the return value is a character
+ */
+char *_strncpy(char *dest, char *src, int n)
+{
+
+	int length;
+	for (length = 0; src[length] != '\0' && length < n; length++)
+	{
+		dest[length] = src[length];
+	}
+	for (; src[length] < n; length++)
+	{
+		dest[length] = '\0';
+	}
+	return (dest);
+}
+

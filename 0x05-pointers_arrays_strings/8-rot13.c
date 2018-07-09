3a7f003281d4f4ad925667f53088c59111c068f4
Wukongzin <2rinzin1@gmail.com>
2018-07-09 03:18:53 +0000

    function that encodes a string using rot13.

---
+++ 0x05-pointers_arrays_strings/8-rot13.c
@@ -0,0 +1,26 @@
+#include "holberton.h"
+/**
+ * *rot13 - function that encodes a string using rot13
+ * @s: s is the pointer of type char
+ * Return: return output value is of type char
+ */
+char *rot13(char *s)
+{
+
+	int i;
+	int z;
+	char inputstring[] ="ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+	char outputstring[] = "NOPQRSTUVWXYZABCDEFGHIJKLMnopqrstuvwxyzabcdefghijklm";
+	
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		for (z= 0; inputstring[z] != '\0'; z++)
+		{	
+			if (s[i] == inputstring[z])
+ 			{		
+				s[i] = outputstring[z];
+			}
+		}
+	}
+	return (s);
+}

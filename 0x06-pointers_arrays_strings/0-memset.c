18721cc88a68c3a4c32f5d028fe3b0f05f657400
Wukongzin <2rinzin1@gmail.com>
2018-07-10 21:26:06 +0000

    function that fills memory with a constant byte

--- 0x06-pointers_arrays_strings/0-memset.c
+++ 0x06-pointers_arrays_strings/0-memset.c
@@ -1,7 +1,7 @@
 #include "holberton.h"
 /**
  * *_memset - function that fills memory with a constant byte
- * @*s: it is the pointer with variable s of type int
+ * @s: it is the pointer with variable s of type int
  * @b: b is the input variable of type char
  * @n: n is the variable of type int
  * Return: return value of the output is of type char
@@ -11,7 +11,7 @@ char *_memset(char *s, char b, unsigned int n)
 
 	unsigned int i;
 
-	for (i = 0; i <= n; i++)
+	for (i = 0; i < n; i++)
 	{
 		s[i] = b;
 	}

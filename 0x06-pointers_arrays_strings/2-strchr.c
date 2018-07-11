af09f24d658dcb29e014b291f98c06e45f3f6df4
Wukongzin <2rinzin1@gmail.com>
2018-07-11 00:45:09 +0000

    be right

--- 0x06-pointers_arrays_strings/2-strchr.c
+++ 0x06-pointers_arrays_strings/2-strchr.c
@@ -10,17 +10,17 @@ char *_strchr(char *s, char c)
 {
 
 	int i;
+
 	for (i = 0; s[i] != '\0'; i++)
 	{
 		if (s[i] == c)
 		{
 			return (s + i);
 		}
-		
+	}
 	if (c == '\0')
-		{
-			return (s + i);
-		}
+	{
+		return (s + i);
 	}
 	return (0);
 

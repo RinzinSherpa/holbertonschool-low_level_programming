0b81428a4bcdd1084b444c60bfab169097c7608a
Wukongzin <2rinzin1@gmail.com>
2018-07-12 05:29:10 +0000

    you need to correct this

---
+++ 0x07-recursion/3-factorial.c
@@ -0,0 +1,22 @@
+#include "holberton.h"
+/**
+ * factorial - function that returns the factorial of a given number
+ * @n: ble s of type char
+ * Return: the output value of return is of type int
+ */
+int factorial(int n)
+{
+	if (n < 0)
+	{
+		return (-1);
+	}
+	else if (n > 0)
+	{
+		return n * factorial (n - 1);
+	
+	}
+	else 
+	{
+		return (0);
+	}
+}

0bc2dafe5ec0c1cacd6355da74e0a23517b8d2f0
Wukongzin <2rinzin1@gmail.com>
2018-07-15 23:26:24 +0000

    I fixed the requirement of the program that prints its name, followed by a new line.

--- 0x09-argc_argv/0-whatsmyname.c
+++ 0x09-argc_argv/0-whatsmyname.c
@@ -1,5 +1,12 @@
 #include <stdio.h>
+#include "holberton.h"
 
+/**
+ * main - program that prints its name followed by a new line
+ * @argc: input parameter is argc of type int
+ * @argv: input parameter is a pointer of type char
+ * Return: return output value is of type int
+ */
 int main(int argc, char *argv[])
 {
 	int i;

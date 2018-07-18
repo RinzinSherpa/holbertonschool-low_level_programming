705b4457770ed9a0a04b5596ecc6ef5a8c5ce6fe
Wukongzin <2rinzin1@gmail.com>
2018-07-18 06:00:10 +0000

    incorrect

---
+++ 0x0A-malloc_free/1-strdup.c
@@ -0,0 +1,36 @@
+#include "holberton.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ *_strdup - function is a pointer of data type char
+ *@str: input parameter is a string of type char
+ * Return: return value of the output is of type char
+ */
+char *_strdup(char *str);
+{
+
+	int i;
+	char *cptr;
+	
+	if (str == NULL)
+        {
+		return(NULL);
+        }
+	for (i = 0; str[i] !='\0'; i++)
+        {
+        	;
+        }
+	cptr = malloc(sizeof(char) * i + 1);
+
+	for (i = 0; str[i] != '\0'; i++)
+	{
+		cptr[i] = str[i];
+	}
+	if (cptr = NULL)
+	{
+		return (NULL);
+	} 
+	return (cptr);
+}
+

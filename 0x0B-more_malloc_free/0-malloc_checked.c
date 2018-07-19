133944ed877aa63fb5874e5f88573f21a26b64f5
Wukongzin <2rinzin1@gmail.com>
2018-07-19 23:34:12 +0000

    corrected the if condition

---
+++ 0x0B-more_malloc_free/0-malloc_checked.c
@@ -0,0 +1,21 @@
+#include "holberton.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ *maloc_checked - function is a pointer to a void
+ *@b: input parameter is of type unsigned int
+ *Return: returns void
+ */
+
+void *malloc_checked(unsigned int b)
+{
+
+	void *ptr;
+	ptr = malloc(b);	
+	if (ptr == NULL)
+	{
+		exit (98);
+	}
+	return (ptr);
+}

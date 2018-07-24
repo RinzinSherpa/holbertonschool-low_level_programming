87c3e8e10115b8112e5456741d9d4b999c2452df
Wukongzin <2rinzin1@gmail.com>
2018-07-24 21:53:09 +0000

    added comments on the input parameter

--- 0x0D-structures_typedef/1-init_dog.c
+++ 0x0D-structures_typedef/1-init_dog.c
@@ -3,9 +3,11 @@
 #include <stdio.h>
 /**
  *init_dog - function initialize a variable of type
+ *@d: d is a pointing to struct dog
  *@name: First member
  *@age: Second member
  *@owner: Third member
+ *Return: returns nothing
 */
 
 void init_dog(struct dog *d, char *name, float age, char *owner)

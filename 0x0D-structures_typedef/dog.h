80fde19417aa5eb70465c3cddcef6cb8b5c07e2c
Wukongzin <2rinzin1@gmail.com>
2018-07-24 03:42:22 +0000

    added the comment line

---
+++ 0x0D-structures_typedef/dog.h
@@ -0,0 +1,18 @@
+#ifndef _dog_H_
+#define _dog_H_
+
+/**
+ *struct dog - Define a new type struct dog
+ * @name: First member
+ * @age: Second member
+ * @owner: Third member
+ */
+
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+#endif

398348e324c3dc62caf75eeb1bdde221a6dbb1a1
Wukongzin <2rinzin1@gmail.com>
2018-07-26 00:19:18 +0000

    changed the header file order

---
+++ 0x0E-function_pointers/1-array_iterator.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include "function_pointers.h"
+/**
+ *array_iterator - executes funct given as parameter on  every array  element
+ **array: is a pointer to int
+ *size: is the size of array
+ **action: is a pointer function pointing to int with void return
+ *Return: returns nothing
+
+*/
+void array_iterator(int *array, size_t size, void (*action)(int))
+{
+
+	unsigned int i;
+
+	if (array && size)
+	{
+		for (i = 0; i < size; i++)
+		{
+			(*action)(array[i]);
+		}
+	}
+}

036c5bb5bc4e14a784b235b781e72e4adf03dd43
Wukongzin <2rinzin1@gmail.com>
2018-07-26 02:21:20 +0000

    corrected the if condition

--- 0x0E-function_pointers/1-array_iterator.c
+++ 0x0E-function_pointers/1-array_iterator.c
@@ -12,11 +12,13 @@ void array_iterator(int *array, size_t size, void (*action)(int))
 
 	unsigned int i;
 
-	if (array && size)
+	if (array == NULL || size == 0 || action == NULL)
 	{
+		return;
+	}
 		for (i = 0; i < size; i++)
 		{
 			(*action)(array[i]);
 		}
-	}
+
 }
